cb2776a9aebfe48bce56eb9a2b850791c3304e36
Reza <reza@ies-italia.it>
2024-08-14 02:56:51 +0200

    init

--- controller.go
+++ controller.go
@@ -5,7 +5,7 @@ import "github.com/getevo/evo/v2"
 // Controller represents a controller type.
 type Controller struct{}
 
-// Models returns a collection of resources.
+// Models returns all registered models.
 func (c Controller) Models(request *evo.Request) interface{} {
 	return resources
 }
