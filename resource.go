e0c30628e4aa65ecb479b871a70904f1a53d24b3
Reza <reza@ies-italia.it>
2025-04-18 14:27:13 +0200

    export resources

--- resource.go
+++ resource.go
@@ -445,7 +445,7 @@ func (context *Context) Error(err error, code int) *Error {
 }
 
 func (context *Context) GetDBO() *gorm.DB {
-	var dbo = db.GetContext(context)
+	var dbo = db.GetContext(context, context.Request)
 	return dbo
 }
 
